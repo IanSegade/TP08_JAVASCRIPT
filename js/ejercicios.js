4e30b0e98a6d692dad5e71d0cd533c6143bbb931
IAN MANUEL SEGADE <48521796@ort>
2024-09-30 09:59:53 -0300

    separarElementosPorComa

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,6 +1,8 @@
+import { ingresarFecha, calcularEdadConFecha, ingresarTexto} from './util.js';
+
 function ejecutarFuncion()
 {
-    let opcion = document.getElementById('opcion');
+    let opcion = document.getElementById('opcion').value;
 
     switch (opcion)
     {
@@ -44,22 +46,41 @@ function ejecutarFuncion()
             compararDatosYtipos();
             break;
     }
+
+
+
 }
 
+window.ejecutarFuncion = ejecutarFuncion; 
 function calcularEdad()
 {
     let fecha = ingresarFecha();
-    calcularEdadConFecha(fecha);
+
+    console.log("Su edad actual es: " + calcularEdadConFecha(fecha));
 }
 
 function calcularRecaudacion()
 {
-
+    let texto = ingresarTexto();
+    //leti:10,tomi:40,ian:90,lucas:50
+    let todoSpliteado;
+    let recaudacion;
+    let listadoPedidos = texto.split(",");
+    //listadoPedidos = ["leti:10" , "tomi:40" , "ian:90" , "lucas:50"]
+    for(let parte of listadoPedidos)
+    {
+        todoSpliteado = parte.split(":");
+        recaudacion += parseInt(todoSpliteado[1]);
+        //"leti" , "10" , "tomi" , "40" , "ian" , "90" , "lucas" , "50"
+    }
+    console.log(recaudacion);
 }
 
 function separarElementosPorComa()
 {
-
+    let listaElementos = ingresarTexto();
+    //leti,tomi,ian,lucas,binker,damo,joi
+    console.log((listaElementos.split(",")).join('-'));
 }
 
 function cortarStringPorNumero()
--- js/util.js
+++ js/util.js
@@ -6,14 +6,20 @@ export function ingresarFecha()
 
 export function calcularEdadConFecha(fecha)
 {
-    fechaActual = new Date();
-    fechaIngresada = new Date (fecha);
-
-    let edad = fechaActual.GetFullYear() - fechaIngresada.GetFullYear();
-    if(fechaActual.GetMonth() > fechaIngresada.GetMonth() | ((fechaActual.GetMonth() = fechaIngresada.GetMonth()) && (fechaActual.GetDay() >= fechaIngresada.GetDay())))
+    let fechaActual = new Date();
+    console.log(fechaActual);
+    let fechaIngresada = new Date(fecha)
+    let edad = fechaActual.getFullYear() - fechaIngresada.getFullYear();
+    if(fechaActual.getMonth() > fechaIngresada.getMonth() || ((fechaActual.getMonth() === fechaIngresada.getMonth()) && (fechaActual.GetDay() >= fechaIngresada.getDay())))
     {
         edad++;
     }
-
+    
     return edad;
+}
+
+export function ingresarTexto()
+{
+    let texto = prompt("Ingrese el texto:");
+    return texto;
 }
